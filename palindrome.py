84056169c25545495f20b44250cf9e2fce427fad
katrinia reppen <reppenkat@gmail.com>
2025-08-03 14:37:05 -0400

    modified to add punctuation clean up

--- palindrome.py
+++ palindrome.py
@@ -6,17 +6,29 @@
 # Removing spaces (and optionally punctuation)
 # Reverse the normalized string and compare it to the original normalized version.
 
+import string
 
 def is_palindrome(input_string):
-    cleanup = input_string.strip().lower() 
-    return cleanup[::-1]
+    # Strip leading/trailing whitespace and convert to lowercase
+    clean = input_string.strip().lower()
+    
+    # Remove all punctuation
+    no_punct = clean.translate(str.maketrans('', '', string.punctuation))
+    
+    # Return both cleaned and reversed strings
+    return no_punct, no_punct[::-1]
 
+# Have user input a word to evaluate
 user_input = input("Enter a string to check if it's a palindrome: ")
-reversed_output = is_palindrome(user_input)
-print(f"The reversed of the string is: {reversed_output}")
 
-if user_input.strip().lower() == reversed_output:
+# Set variables to values returned in the function
+cleaned, reversed_output = is_palindrome(user_input)
+
+print(f"The cleaned string from entered string is: {cleaned}\nThe reverse of the cleaned string is: {reversed_output}")
+
+if cleaned == reversed_output:
     print("Yes, the string is a palindrome.")
 else:
     print("No, the string is not a palindrome.")
+
 
